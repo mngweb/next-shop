f3c0a108309fa3514dd1ae5426f7ae222b5d8c28
Sylwia <mangoweb@wp.pl>
2022-06-22 19:46:58 +0200

    create products page with CSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div>
-    <Component {...pageProps} />
-  </div>
+  return (
+    <div>
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/products-csr.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from 'react-query';
+import { Product } from '../components/Product';
+
+const getProducts = async () => {
+  const res = await fetch(`https://fakestoreapi.com/products/`);
+  const data: StoreApiResponse[] = await res.json();
+  return data;
+};
+
+const ProductsCSRPage = () => {
+  const { isLoading, data, error } = useQuery('products', getProducts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || error) {
+    return <div>Something went wrong.</div>;
+  }
+
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {data.map((product) => {
+        return (
+          <li key={product.id}>
+            <Product
+              data={{
+                title: product.title,
+                description: product.description,
+                imageUrl: product.image,
+                imageAlt: product.title,
+                rating: product.rating.rate,
+              }}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ProductsCSRPage;
+
+export interface StoreApiResponse {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
